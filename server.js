f47eaa1f81cb321fd5dbcf1a3eac8ef041234c30
yohanessahrul <yohanessahrul92@gmail.com>
2018-10-29 17:02:16 +0700

    SSR manipulate SEO

--- server.js
+++ server.js
@@ -10,6 +10,7 @@ app.get('/', function (req, res) {
   console.log('/')
   fs.readFile(filePath, 'utf8', function(err, data) {
     if (err) { return console.log(err); }
+    data = data.replace(/\$OG_TITLE/g, 'Klinik KTA | Pelayanan Kesehatan Anda');
     res.send(data);
   })
 })
@@ -17,6 +18,7 @@ app.get('/klinik-taman-anggrek', function (req, res) {
     console.log('/klinik-taman-anggrek')
     fs.readFile(filePath, 'utf8', function(err, data) {
       if (err) { return console.log(err); }
+      data = data.replace(/\$OG_TITLE/g, 'Klinik Taman Anggrek');
       res.send(data);
     })
 })
@@ -24,6 +26,7 @@ app.get('/klinik-kta-srengseng-junction', function (req, res) {
     console.log('/klinik-kta-srengseng-junction')
     fs.readFile(filePath, 'utf8', function(err, data) {
       if (err) { return console.log(err); }
+      data = data.replace(/\$OG_TITLE/g, 'Klinik KTA Srengseng Junction');
       res.send(data);
     })
 })
@@ -31,6 +34,7 @@ app.get('/klinik-syifa-medika', function (req, res) {
     console.log('/klinik-syifa-medika')
     fs.readFile(filePath, 'utf8', function(err, data) {
       if (err) { return console.log(err); }
+      data = data.replace(/\$OG_TITLE/g, 'Klinik Syifa Medika');
       res.send(data);
     })
 })
@@ -38,6 +42,7 @@ app.get('/klinik-kta-surya-cipta', function (req, res) {
     console.log('/klinik-kta-surya-cipta')
     fs.readFile(filePath, 'utf8', function(err, data) {
       if (err) { return console.log(err); }
+      data = data.replace(/\$OG_TITLE/g, 'Klinik KTA Surya Cipta');
       res.send(data);
     })
 })
